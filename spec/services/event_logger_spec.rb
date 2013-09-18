2eae4ffd1023bd0c452b9b3493c2fa82d0067788
Hector V <hectorvs@gmail.com>
2013-09-18 15:23:32 -0500

    working on event logger spec

--- spec/services/event_logger_spec.rb
+++ spec/services/event_logger_spec.rb
@@ -7,8 +7,17 @@ describe Subscriptions::EventLogger do
     let(:payment) { FactoryGirl.create(:payment, invoice: invoice, customer: customer ) }
 
     it "should log a new charge with all data" do
-
       EventLogger.log(payment.id, payment.customer.email, "charged", payment.amount, "some raw data", "charge", "Authorize.net")
+
+      generated_event = payment.event
+
+      generated_event.payment.id.should eq(payment.id)
+      generated_event.user.should eq(payment.customer.email)
+      generated_event.action.should eq("charged")
+      generated_event.amount.should eq(payment.amount)
+      generated_event.data.should eq("some raw data")
+      generated_event.type.should eq("charge")
+      generated_event.source.should eq("Authorize.net")
 
     end
   end
