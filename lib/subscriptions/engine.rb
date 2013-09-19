be5513dc6ee62710adb16356588cd77b8f39660a
Hector V <hectorvs@gmail.com>
2013-09-19 16:02:46 -0500

    removing the auto copy of migrations

--- lib/subscriptions/engine.rb
+++ lib/subscriptions/engine.rb
@@ -1,14 +1,5 @@
 module Subscriptions
   class Engine < ::Rails::Engine
     isolate_namespace Subscriptions
-
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
-
   end
 end
