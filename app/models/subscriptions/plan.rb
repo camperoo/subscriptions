7a53246c11cab8adf71bc140a53f31f1d6db5e6a
Hector V <hectorvs@gmail.com>
2013-10-02 17:07:07 -0500

    adding validations to plan

--- app/models/subscriptions/plan.rb
+++ app/models/subscriptions/plan.rb
@@ -1,5 +1,20 @@
 module Subscriptions
   class Plan < ActiveRecord::Base
     belongs_to :group
+
+    validates :interval_units, :interval_quantity, :name, :amount, presence: true
+    validates :amount, numericality: true
+
+
+    def description
+      quantity_string = ""
+      if interval_quantity == 1
+        quantity_string = "/"
+      else
+        quantity_string = "every #{interval_quantity}"
+      end
+
+      "$#{amount} #{quantity_string} #{interval_units.pluralize(interval_quantity)}"
+    end
   end
 end
